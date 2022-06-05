a37e986c2f9b7d0a46471a8b176fd017272f1adf
xndybxy598 <84103424+xndybxy598@users.noreply.github.com>
2022-06-05 18:41:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -14,5 +14,25 @@ recognition.onresult = function(event){
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
 
